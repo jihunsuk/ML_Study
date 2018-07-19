4f80fc81be80e39b5c28220289b754b81a0c7d62
tjrwlgns1198 <tjrwlgns1198@gmail.com>
2018-07-19 14:43:56 +0900

    revised

--- lab1_Hello.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-hello = tf.constant("Hello, TensorFlow!")   #텐서플로우 노드 생성
-sess = tf.Session()
-print(sess.run(hello))  #노드실행
-
-#b'Hello, TensorFlow!'라고 출력된다.  b라는것은 byte String이라는 의미
